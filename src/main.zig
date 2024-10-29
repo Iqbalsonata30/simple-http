87a2bc3b8ea630e34432b1ef6cf96f4cf6b4a090
iqbalsonata <iqbalsonata2@gmail.com>
2024-10-29 19:12:31 +0700

    created tcp connection

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,50 @@
 const std = @import("std");
+const net = std.net;
+const posix = std.posix;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var address = try net.Address.parseIp4("127.0.0.1", 8080);
+    const listener = posix.socket(posix.AF.INET, posix.SOCK.STREAM, 0) catch |err| {
+        std.debug.print("error created socket: {?}\n", .{err});
+        return err;
+    };
+    defer posix.close(listener);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    _ = posix.bind(listener, @ptrCast(&address), address.getOsSockLen()) catch |err| {
+        std.debug.print("failed to bind socket: {?}\n", .{err});
+        return err;
+    };
+    _ = posix.listen(listener, 10) catch |err| {
+        std.debug.print("failed to listen socket: {?}\n", .{err});
+        return err;
+    };
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    std.debug.print("server listening on {any}\n", .{address.in});
+    var client_address: net.Address = undefined;
+    var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+    while (true) {
+        const conn = posix.accept(listener, @ptrCast(&client_address), &client_address_len, 0) catch |err| {
+            std.debug.print("failed to accept connection: {?}\n", .{err});
+            continue;
+        };
 
-    try bw.flush(); // don't forget to flush!
+        const thread = try std.Thread.spawn(.{}, handleConnection, .{conn});
+        defer thread.join();
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn handleConnection(conn: posix.socket_t) !void {
+    std.debug.print("client connected\n", .{});
+    const response = "GET / HTTP/1.1 200 OK\r\n" ++
+        "Content-Type: text/html\r\n" ++
+        "Content-Length: 12\r\n" ++
+        "Connection: keep-alive \r\n\r\n" ++
+        "<h1>Hello World</h1>";
+    const written_size = try posix.write(conn, response);
+    std.debug.print("written size : {any}\n", .{written_size});
 }
+//var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+// defer arena.deinit();
+// const allocator = arena.allocator();
+// try allocator.alloc(posix.socket_t, client_sock);
+// defer allocator.free(memory: anytype)
